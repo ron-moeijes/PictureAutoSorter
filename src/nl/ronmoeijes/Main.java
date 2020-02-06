27f32e1a7f734a4660d7fe146fa72131b4c824d5
ronimoses <ron.moeijes@outlook.com>
2020-02-06 01:42:12 +0100

    Reworking moveFile functions

--- src/nl/ronmoeijes/Main.java
+++ src/nl/ronmoeijes/Main.java
@@ -2,10 +2,10 @@ package nl.ronmoeijes;
 
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
+import com.drew.lang.annotations.NotNull;
 import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.Tag;
-import com.sun.istack.internal.NotNull;
 import org.apache.commons.cli.*;
 
 import java.io.File;
@@ -23,7 +23,6 @@ import java.util.stream.Stream;
 
 import static java.time.format.DateTimeFormatter.*;
 import static java.util.Objects.nonNull;
-import static java.util.regex.Pattern.matches;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 
 public class Main {
@@ -31,97 +30,113 @@ public class Main {
     private static final Path PICTURE_ROOT = Paths.get("D:\\Afbeeldingen\\Foto's");
     private static final String DATE_TIME_REGEX = "\\d{4}:\\d{2}:\\d{2} \\d{2}:\\d{2}:\\d{2}";
     private static final String SUFFIX_REGEX = "\\d{2,4}\\W\\d{2}\\W\\d{2,4} ";
+    private static final String UNDERSCORE = "_";
     private static final DateTimeFormatter YEAR_PATTERN = DateTimeFormatter.ofPattern("yyyy");
-    private static final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("yyyyMMdd");
+    private static final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("MMdd");
     private static final DateTimeFormatter TIME_PATTERN = DateTimeFormatter.ofPattern("HHmmss");
-    private static final DateTimeFormatter META_DATA_FORMATTER = DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ss");
+    private static final DateTimeFormatter META_DATA_PATTERN = DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ss");
+    private static final String DASHES = "----------------------------------------"
+            + "----------------------------------------"
+            + "----------------------------------------";
     private static List<String> years = new ArrayList<>();
     private static List<String> dates = new ArrayList<>();
     private static List<String> times = new ArrayList<>();
     private static Path source;
     private static Path target;
     private static boolean addPrefix;
+    private static boolean addSuffix;
     private static String suffix = EMPTY;
 
     public static void main(String[] args) throws IOException {
         // write your code here
 
-        // create the command line parser
-        CommandLineParser parser = new DefaultParser();
+        parseOptions(args);
 
-        // create the Options
-        Options options = new Options();
-        options.addOption( "p", "prefix", false, "add a prefix for each file" );
-        Option suffixOption = Option.builder("x")
-                .longOpt( "suffix" )
-                .desc( "specify a suffix for the new directory [Default = name of previous parent directory]" )
-                .optionalArg(true)
-                .argName( "SUFFIX" )
-                .build();
-        options.addOption( suffixOption );
-        Option sourceOption = Option.builder("s")
-                .longOpt( "source" )
-                .desc( "specify a source path"  )
-                .hasArg()
-                .argName( "SOURCE" )
-                .build();
-        options.addOption( sourceOption );
-        Option targetOption = Option.builder("t")
-                .longOpt( "target" )
-                .desc( "specify a target path"  )
-                .hasArg()
-                .argName( "TARGET" )
-                .build();
-        options.addOption( targetOption );
+        System.out.println("SOURCE: " + source);
+        analyseDates(source);
+        target = appendSuffix(years.get(0));
+        System.out.println("TARGET: " + target);
+        createDirectory(target);
+        moveFiles(source, target);
+    }
+
+    private static void parseOptions(String[] args) {
+        // create the command line parser
+//        step("parsing options");
+        CommandLineParser parser = new DefaultParser();
 
         try {
             // parse the command line arguments
-            CommandLine line = parser.parse( options, args );
+            CommandLine line = parser.parse(createOptions(), args);
 
             // validate that TARGET_DIR has been set
-            if( line.hasOption( "target" ) ) {
+            if (line.hasOption("target")) {
                 target = Paths.get(line.getOptionValue("target"));
             } else {
                 target = PICTURE_ROOT;
             }
             source = Paths.get(line.getOptionValue("source"));
-            if( line.hasOption( "prefix" ) ) {
+            if (line.hasOption("prefix")) {
                 addPrefix = true;
             }
-            if( line.hasOption( "suffix" ) ) {
-                suffix = "_" + line.getOptionValue("suffix");
+            if (line.hasOption("suffix")) {
+                addSuffix = true;
+                suffix = UNDERSCORE + line.getOptionValue("suffix");
                 if (suffix.equals("_null")) {
-                    suffix = "_" + source.getFileName();
-                    suffix = suffix.replaceAll(SUFFIX_REGEX, EMPTY);
+                    suffix = EMPTY;
                 }
             }
+        } catch (ParseException exp) {
+            System.out.println("Unexpected exception:" + exp.getMessage());
         }
-        catch(ParseException exp ) {
-            System.out.println( "Unexpected exception:" + exp.getMessage() );
-        }
-        System.out.println("SOURCE: " + source);
-//        System.out.println("TARGET: " + TARGET_DIR + "\\" + DATE_PLACEHOLDER);
-        analyseDates(source);
+    }
+
+    private static Options createOptions() {
+        // create the Options
+//        step("creating options");
+        Options options = new Options();
+        options.addOption("p", "prefix", false, "add a prefix for each file");
+        Option suffixOption = Option.builder("x")
+                .longOpt("suffix")
+                .desc("specify a suffix for the new directory [Default = name of previous parent directory]")
+                .hasArg()
+                .argName("SUFFIX")
+                .build();
+        options.addOption(suffixOption);
+        Option sourceOption = Option.builder("s")
+                .longOpt("source")
+                .desc("specify a source path")
+                .hasArg()
+                .argName("SOURCE")
+                .build();
+        options.addOption(sourceOption);
+        Option targetOption = Option.builder("t")
+                .longOpt("target")
+                .desc("specify a target path")
+                .hasArg()
+                .argName("TARGET")
+                .build();
+        options.addOption(targetOption);
+        return options;
     }
 
     private static boolean analyseDates(Path source) throws IOException {
+        step("analysing dates");
         try (Stream<Path> paths = Files.walk(source)) {
             paths
                     .filter(Files::isRegularFile)
-                    .forEach(path -> {
-                        LocalDateTime dateTime = fetchDateTime(path);
-                        addDateTimeToList(dateTime,YEAR_PATTERN,years);
-                        addDateTimeToList(dateTime,DATE_PATTERN,dates);
-                        addDateTimeToList(dateTime,TIME_PATTERN,times);
+                    .forEach(file -> {
+                        LocalDateTime dateTime = fetchDateTime(file);
+                        addDateTimeToList(dateTime, YEAR_PATTERN, years);
+                        addDateTimeToList(dateTime, DATE_PATTERN, dates);
+                        addDateTimeToList(dateTime, TIME_PATTERN, times);
                     });
         }
-        System.out.println(years);
-        System.out.println(dates);
-        System.out.println(times);
         return !(dates.size() == 1) && years.size() == 1;
     }
 
     private static LocalDateTime fetchDateTime(Path path) {
+//        step("fetching datetime");
         File file = path.toFile();
         // There are multiple ways to get a Metadata object for a file
 
@@ -146,7 +161,7 @@ public class Main {
                 for (Tag tag : directory.getTags()) {
                     if (tag.getTagName().equals("Date/Time Original")) {
 
-                        return LocalDateTime.parse(tag.getDescription(), META_DATA_FORMATTER);
+                        return LocalDateTime.parse(tag.getDescription(), META_DATA_PATTERN);
                     }
                 }
             }
@@ -160,63 +175,67 @@ public class Main {
             return ZonedDateTime.parse(lastModifiedTime, ISO_DATE_TIME).toLocalDateTime();
         } catch (ImageProcessingException e) {
             System.err.println("ERROR: " + e + " for " + file.getName());
+            return null;
         } catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
-        return null;
     }
 
-    private static void addDateTimeToList(LocalDateTime dateTime, DateTimeFormatter dateTimeFormatter, List<String> list){
+    private static void addDateTimeToList(LocalDateTime dateTime, DateTimeFormatter dateTimeFormatter, List<String> list) {
+//        step("add datetime to list");
         if (nonNull(dateTime)) {
             String formattedDateTime = dateTime.format(dateTimeFormatter);
             if (!list.contains(formattedDateTime)) {
                 list.add(formattedDateTime);
-                System.out.println(formattedDateTime);
             }
         }
     }
 
-    // Move file
-    private static void moveFiles(Path source, Path target) throws IOException {
-        Files.move(source, target);
-        if (source.equals(target)) {
-            System.out.println("INFO: " + source.getFileName() + " was already in " + source.getParent());
-        } else {
-            System.out.println("MOVED: " + source.getFileName() + " moved from " + source + " to " + target);
+    private static Path appendSuffix(String date) {
+        if (addSuffix) {
+            return target.resolve(date + suffix);
         }
+        return target.resolve(date);
     }
 
-    // Create directory
     private static void createDirectory(Path target) {
+        // Create directory
+        step("creating directory");
         File targetDir = target.toFile();
-        if (targetDir.mkdir()) {
-            System.out.println("TARGET: " + targetDir);
+//        if (targetDir.mkdir()) {
+//            System.out.println("TARGET: " + targetDir);
+//        }
+        System.out.println(targetDir);
+    }
+
+    private static void moveFiles(Path source, Path target) throws IOException {
+        step("moving files");
+        try (Stream<Path> paths = Files.walk(source)) {
+            paths
+                    .filter(Files::isRegularFile)
+                    .forEach(file -> {
+                        LocalDateTime dateTime = fetchDateTime(file);
+                        moveFile(file, target, dateTime);
+                    });
         }
     }
 
-//    // Loop directory
-//    private static void createDateDirectories(Path source, @NotNull Path target, boolean addPrefix, String suffix) throws IOException {
-//        try (Stream<Path> paths = Files.walk(source)) {
-//            paths
-//                    .filter(Files::isRegularFile)
-//                    .forEach(path -> {
-//                        try {
-//                            String dateTime = fetchDateTime(path);
-//                            Path datePath = createDateTimePath(dateTime, suffix);
-//                            Path targetPath = target.resolve(datePath);
-//                            createDirectory(targetPath);
-//                            Path filePath;
-//                            if (addPrefix) {
-//                                Path prefix = createDateTimePath(dateTime);
-//                                filePath = targetPath.resolve(prefix + "_" + path.getFileName());
-//                            } else {
-//                                filePath = targetPath.resolve(path.getFileName());
-//                            }
-//                            moveFiles(path, filePath);
-//                        } catch (IOException e) {
-//                            e.printStackTrace();
-//                        }
-//                    });
-//        }
-//    }
+    // Move file
+    private static void moveFile(Path file, Path target, LocalDateTime dateTime) {
+        String fileName = file.getFileName().toString();
+        if (file.equals(target)) {
+            System.out.println("INFO: " + fileName + " was already in " + file.getParent());
+        } else if (nonNull(dateTime)) {
+            System.out.println("MOVED: " + file + " to " + target.resolve(dateTime.format(DATE_PATTERN)).resolve(dateTime.format(TIME_PATTERN) + UNDERSCORE + fileName));
+        } else {
+            System.out.println("MOVED: " + fileName + " moved from " + file.getParent() + " to " + target.resolve("Other files"));
+        }
+    }
+
+    private static void step(String output) {
+        System.out.println(DASHES);
+        System.out.println(output.toUpperCase());
+        System.out.println(DASHES);
+    }
 }
